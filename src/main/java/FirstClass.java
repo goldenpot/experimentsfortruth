181a97fb61553cec45cd9379effdf8a3d75df780
pearlpret <pearlpret@gmail.com>
2021-11-05 16:59:09 -0400

    scanner

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hare Krishna");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the array size: ");
+        int size = scan.nextInt();
+        int[] yourArray = new int[size];
+    }
+}
