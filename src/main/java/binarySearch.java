9da895bc2cad12a62b6ecb6d14d2a196049a514e
pearlpret <pearlpret@gmail.com>
2022-01-27 20:14:17 -0500

    scanner 3

---
+++ src/main/java/binarySearch.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class binarySearch {
+
+     public static void main(String args[]){
+
+         Integer[] spam = new Integer[] { 1, 2, 3 };
+         List<Integer> list = Arrays.asList(spam);
+
+
+         System.out.println("Hare Krishna "+list.size());
+     }
+}
